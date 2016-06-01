f2d258834ef237829f2371fb198d90ab5b7e228b
sadcookie <lee10241993@gmail.com>
2016-06-01 17:48:02 +0900

    comiit

--- src/Main.java
+++ src/Main.java
@@ -5,46 +5,47 @@ public class Main {
 	public static void main(String[] args) {
 		int num = new Scanner(System.in).nextInt();
 		int room[] = new int[num];
-		String a[] = new String[6];
+		int val[] = new int[6];
 		for (int i = 0; i < num; i++) {
-			String val = new Scanner(System.in).nextLine();
-			int length = val.length();
-			a = (val.split(" "));
-			int b = (Integer.valueOf(a[2])+Integer.valueOf(a[5]))*
-					(Integer.valueOf(a[2])+Integer.valueOf(a[5]));
-			int c = (Integer.valueOf(a[2])-Integer.valueOf(a[5]))*
-					(Integer.valueOf(a[2])-Integer.valueOf(a[5]));
-			if (Integer.valueOf(a[0]) != Integer.valueOf(a[3]) || Integer.valueOf(a[1]) != Integer.valueOf(a[4])) {
-				int len = ((Integer.valueOf(a[0]) - Integer.valueOf(a[3]))
-						* (Integer.valueOf(a[0]) - Integer.valueOf(a[3])))
-						+ ((Integer.valueOf(a[1]) - Integer.valueOf(a[4]))
-								* (Integer.valueOf(a[1]) - Integer.valueOf(a[4])));
-				if(len==b){
-					room[i]=1;
-				}else if(len>b){
-					if(len==c){
-					room[i]=1;
-	
-				}else
-					room[i]=0;
-				}else if(len<b){
-					if(len==c){
-					room[i]=1;
-					
-				}else
-					room[i]=2;
+			String s = new Scanner(System.in).nextLine();
+			String[] e=s.split(" ");
+			
+			for(int x=0;x<6;x++){
+				val[x]=Integer.valueOf(e[x]);
+			}
+			//두 중심 사이 거리
+			int len = ((((val[0]-val[3])*(val[0]-val[3]))
+					+ ((val[1]-val[4])*(val[1]-val[4]))));
+			
+			int b = ((val[2] + val[5])*(val[2] + val[5]));
+			//c는 두개의 반지름 길이 뺀거 제곱
+			int c = ((val[2] - val[5])*(val[2] - val[5]));
+			if (len==0){
+				if (val[2]==val[5]) {
+					room[i] = -1;
+				} else {
+					room[i] = 0;
 				}
-				
-			}else {
-				if(Integer.valueOf(a[2])==Integer.valueOf(a[5])){
-					room[i]=-1;
-				}else{
+
+			} else {
+				if (len >b) {
 					room[i]=0;
+				}else if(len==b){
+					room[i]=1;
+				}else{
+					if(len==c){
+						room[i]=1;
+					}else if(len<c){
+						room[i]=0;
+					}else if(len>2){
+						room[i]=2;
+					}else
+						room[i]=2;
 				}
 			}
 		}
-		for(int i=0;i<num;i++)
-		System.out.println(room[i]);
+		for (int i = 0; i < num; i++)
+			System.out.println(room[i]);
 	}
 
 }
